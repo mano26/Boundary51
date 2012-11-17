9a101fdec5d5bb28e5998b31cf4ae4abf5d12102
Alex Hill <ahill1994@student.elgin.edu>
2012-11-16 19:09:14 -0600

    Created user and session models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Demo::Application.routes.draw do
 
+
+  get '/login' => 'sessions#new', as: 'login'
+  get '/logout' => 'sessions#logout', as: 'logout'
+  post '/sessions' => 'sessions#create'
+
+  
+  resources :users
+
   resources :landings
   resources :listings
 
