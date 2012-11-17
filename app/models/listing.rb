f5b157e576e988a70d3603a61310b88840372c11
Erica Mazza <erica.mazza1@gmail.com>
2012-11-16 18:11:38 -0600

    changed process_geocoding to false

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,9 +1,13 @@
+# require "gmaps"
+
 class Listing < ActiveRecord::Base
   attr_accessible :address, :available, :bath, :beds, :building_description, :cats, :city, :country, :dogs, :heat_included, :landlord_id, :latitude, :laundry, :longitude, :parking, :price, :showing_instructions, :state, :unit_description, :unit_number, :utilities_included, :zip, :photo_url
 
-   acts_as_gmappable :process_geocoding => true
+   acts_as_gmappable :process_geocoding => false
    
-   def gmaps4rails_address
+  def gmaps4rails_address
      "#{self.latitude}, #{self.longitude}"
-end
+  end
+  
+  
 end
