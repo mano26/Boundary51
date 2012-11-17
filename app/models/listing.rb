2870c67e856fdb9fdc545578f0d2107212f2d7a2
michael romano <mikeromano@gmail.com>
2012-11-16 18:15:49 -0600

    added photo urls to seed file

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,6 +3,7 @@
 class Listing < ActiveRecord::Base
   attr_accessible :address, :available, :bath, :beds, :building_description, :cats, :city, :country, :dogs, :heat_included, :landlord_id, :latitude, :laundry, :longitude, :parking, :price, :showing_instructions, :state, :unit_description, :unit_number, :utilities_included, :zip, :photo_url
 
+
    acts_as_gmappable :process_geocoding => false
    
   def gmaps4rails_address
