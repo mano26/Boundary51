89c224bb80f997d35673a7ab5ddbf1bc0f62b800
Alex Hill <ahill1994@student.elgin.edu>
2012-11-17 00:56:21 -0600

    Added wishlist join model and displayed wishlist in user show page

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,10 +4,13 @@ class Listing < ActiveRecord::Base
   attr_accessible :address, :available, :bath, :beds, :building_description, :cats, :city, :country, :dogs, :heat_included, :landlord_id, :latitude, :laundry, :longitude, :parking, :price, :showing_instructions, :state, :unit_description, :unit_number, :utilities_included, :zip, :photo_url
 
 
-   acts_as_gmappable :process_geocoding => false
-   geocoded_by :address
+  has_many :wishlists
+  has_many :users, :through => :wishlists
 
-   after_validation :geocode
+  acts_as_gmappable :process_geocoding => false
+  geocoded_by :address
+
+  after_validation :geocode
 
    
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   has_secure_password
 
   has_many :listings
+  has_many :wishlists
+  has_many :listings, :through => :wishlists
+
 end
+
