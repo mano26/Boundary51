82d4884c554ccffccba599742944932a8a6ed627
Ryan Teague <rmacteague@gmail.com>
2012-12-08 11:29:34 -0600

    updated logo, removed saved and fav from nav, implemented stacked slide in listings, updated image for rent, full partialed css, deleted unneeded css files, updated slide.js to size properly on collapse

--- app/assets/javascripts/slide.js
+++ app/assets/javascripts/slide.js
@@ -10,12 +10,13 @@ function slidePanel(action){
 
     if(action === 'close'){
         $('.listing').stop().animate({'width' : '35%'});
-        $('.list-photo').stop().animate({'width' : '20%'});
+        $('.list-photo').stop().animate({'width' : '0%'});
         $('#map-arrow').attr('class', 'arrow-right');
-        $('.list-details').stop().animate({'width' : '50%'});
+        $('.list-details').stop().animate({'width' : '70%'});
         $('.list-map').stop().animate({'width' : '60%'});
         $('.gmaps4rails_map').attr({'width' : '100%'});
         $('.list-photo').css({'display' : 'none'});
+        $('.listedby').css({'display' : 'none'});
         $('.map_container').attr({'width' : '100%'});
         $('#maparr').attr('class', 'goright');
         $("#maparr")[0].setAttribute("onclick","slidePanel('open')");
@@ -34,6 +35,7 @@ function slidePanel(action){
         $('.list-map').stop().animate({'width' : '38%'});
         $('.list-photo').stop().animate({'width' : '30%'});
         $('.list-photo').css({'display' : 'block'});
+        $('.listedby').css({'display' : 'none'});
     }
 }
 
@@ -87,9 +89,29 @@ function slidePaneloption(action){
 // }, 0);
 
 $(function(){
-    $("#listhold1 div.stack1").hover(function(){
-        $(this).stop().animate({top:"-130px"},{queue:false,duration:200});
+    $("#listhold1 div.stack2").hover(function(){
+        $(this).stop().animate({top:"135px"}, "slow");
+        $(".listholder").css({'opacity' : '.45'});
+        $(".stackadd").css({'display' : 'none'});
+        $(".list-photo2").css({'display' : 'block'});
     }, function() {
-        $(this).stop().animate({top:"0px"},{queue:false,duration:200});
+        $(this).stop().animate({top:"35px"},{queue:false,duration:200});
+        $(".listholder").css({'opacity' : '1'});
+        $(".stackadd").css({'display' : 'block'});
+        $(".list-photo2").css({'display' : 'none'});
+    });
+});
+
+$(function(){
+    $("#listhold1 div.stack1").hover(function(){
+        $(this).stop().animate({top:"170px"}, "slow");
+        $(".listholder").css({'opacity' : '.45'});
+        $(".stackadd").css({'display' : 'none'});
+        $(".list-photo1").css({'display' : 'block'});
+    }, function() {
+        $(this).stop().animate({top:"70px"},{queue:false,duration:200});
+        $(".listholder").css({'opacity' : '1'});
+        $(".stackadd").css({'display' : 'block'});
+        $(".list-photo1").css({'display' : 'none'});
     });
 });
